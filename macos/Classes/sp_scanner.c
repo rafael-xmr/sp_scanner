c4f0368fa3125715eb495f2c4b31db3169e146a5
Rafael Saes <git@rafael.saes.dev>
2024-05-14 17:34:46 -0300

    feat: ios macos

--- macos/Classes/sp_scanner.c
+++ macos/Classes/sp_scanner.c
@@ -1,3 +1,1 @@
-// Relative import to be able to reuse the C sources.
-// See the comment in ../{projectName}}.podspec for more information.
-#include "../../src/sp_scanner.c"
+// This is an empty file to force CocoaPods to create a framework.
